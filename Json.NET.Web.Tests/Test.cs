aed6909383c87ec541f063a89e0f18fe62275942
Matteo Bruni <ar3s@icloud.com>
2016-05-23 16:11:57 +0200

    tests

---
+++ Json.NET.Web.Tests/Test.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using Newtonsoft.Json;
+namespace Json.NET.Web.Tests
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void TestGet()
+		{
+			var ws = new TestWebServer();
+
+			ws.Start();
+
+			while (!ws.IsStarted) { }
+
+			JsonWebClient.GetAsync<object>($"{TestWebServer.TestHost}?key1=test&key2=try");
+
+			ws.Stop();
+		}
+
+		[Test]
+		public void TestPost()
+		{
+			var ws = new TestWebServer();
+
+			ws.Start();
+
+			while (!ws.IsStarted) { }
+
+			JsonWebClient.PostAsync<object>(TestWebServer.TestHost, "key1=test&key2=try");
+
+			ws.Stop();
+		}
+
+		[Test]
+		public void TestPut()
+		{
+			var ws = new TestWebServer();
+
+			ws.Start();
+
+			while (!ws.IsStarted) { }
+
+			JsonWebClient.PutAsync<object>(TestWebServer.TestHost, "key1=test&key2=try");
+
+			ws.Stop();
+		}
+
+		[Test]
+		public void TestDelete()
+		{
+			var ws = new TestWebServer();
+
+			ws.Start();
+
+			while (!ws.IsStarted) { }
+
+			JsonWebClient.DeleteAsync<object>($"{TestWebServer.TestHost}?key1=test&key2=try");
+
+			ws.Stop();
+		}
+	}
+}
+
