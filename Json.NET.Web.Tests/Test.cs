e902a777d5cc0d9975c3e204fb473b3a2262957c
Matteo Bruni <matteo.bruni@me.com>
2016-05-23 17:03:26 +0200

    spaces fix

--- Json.NET.Web.Tests/Test.cs
+++ Json.NET.Web.Tests/Test.cs
@@ -4,64 +4,64 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 namespace Json.NET.Web.Tests
 {
-	[TestFixture]
-	public class Test
-	{
-		[Test]
-		public async Task TestGet()
-		{
-			var ws = new TestWebServer();
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public async Task TestGet()
+        {
+            var ws = new TestWebServer();
 
-			ws.Start();
+            ws.Start();
 
-			while (!ws.IsStarted) { }
+            while (!ws.IsStarted) { }
 
-			Console.WriteLine(JsonConvert.SerializeObject(await JsonWebClient.GetAsync<object>($"{TestWebServer.TestHost}?key1=test&key2=try")));
+            Console.WriteLine(JsonConvert.SerializeObject(await JsonWebClient.GetAsync<object>($"{TestWebServer.TestHost}?key1=test&key2=try")));
 
-			ws.Stop();
-		}
+            ws.Stop();
+        }
 
-		[Test]
-		public async Task TestPost()
-		{
-			var ws = new TestWebServer();
+        [Test]
+        public async Task TestPost()
+        {
+            var ws = new TestWebServer();
 
-			ws.Start();
+            ws.Start();
 
-			while (!ws.IsStarted) { }
+            while (!ws.IsStarted) { }
 
             Console.WriteLine(JsonConvert.SerializeObject(await JsonWebClient.PostAsync<object>(TestWebServer.TestHost, "key1=test&key2=try")));
 
-			ws.Stop();
-		}
+            ws.Stop();
+        }
 
-		[Test]
-		public async Task TestPut()
-		{
-			var ws = new TestWebServer();
+        [Test]
+        public async Task TestPut()
+        {
+            var ws = new TestWebServer();
 
-			ws.Start();
+            ws.Start();
 
-			while (!ws.IsStarted) { }
+            while (!ws.IsStarted) { }
 
             Console.WriteLine(JsonConvert.SerializeObject(await JsonWebClient.PutAsync<object>(TestWebServer.TestHost, "key1=test&key2=try")));
 
-			ws.Stop();
-		}
+            ws.Stop();
+        }
 
-		[Test]
-		public async Task TestDelete()
-		{
-			var ws = new TestWebServer();
+        [Test]
+        public async Task TestDelete()
+        {
+            var ws = new TestWebServer();
 
-			ws.Start();
+            ws.Start();
 
-			while (!ws.IsStarted) { }
+            while (!ws.IsStarted) { }
 
             Console.WriteLine(JsonConvert.SerializeObject(await JsonWebClient.DeleteAsync<object>($"{TestWebServer.TestHost}?key1=test&key2=try")));
 
-			ws.Stop();
-		}
-	}
+            ws.Stop();
+        }
+    }
 }
 
